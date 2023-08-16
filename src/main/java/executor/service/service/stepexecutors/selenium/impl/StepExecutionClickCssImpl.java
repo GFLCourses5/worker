8c03f78024327de4ca8ab9f1a3d184b345dc7bc8
Sokol3783 <sokolplus3783@gmail.com>
2023-08-16 20:02:26 +0300

    do implementation of StepExecutionClickCss

--- src/main/java/executor/service/service/stepexecutors/selenium/impl/StepExecutionClickCssImpl.java
+++ src/main/java/executor/service/service/stepexecutors/selenium/impl/StepExecutionClickCssImpl.java
@@ -1,17 +1,21 @@
 package executor.service.service.stepexecutors.selenium.impl;
 
 import executor.service.model.entity.Step;
+import executor.service.service.stepexecutors.selenium.SeleniumStepExecutionFactory.SeleniumAction;
 import executor.service.service.stepexecutors.selenium.StepExecutionClickCss;
+import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
 
 public class StepExecutionClickCssImpl implements StepExecutionClickCss {
     @Override
     public String getStepAction() {
-        return "do smth";
+        return SeleniumAction.CLICK_CSS.getName();
     }
 
     @Override
     public void step(WebDriver webDriver, Step step) {
-        step.setValue("1");
+        WebElement element = webDriver.findElement(By.cssSelector(step.getValue()));
+        element.click();
     }
 }
