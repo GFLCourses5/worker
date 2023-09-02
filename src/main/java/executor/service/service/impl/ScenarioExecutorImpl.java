2285ddffcda7ccdd8500a79b2e6f568b24acfb44
ykotsiuba <jurok3x@gmail.com>
2023-09-02 23:37:23 +0300

    added scenario executor

--- src/main/java/executor/service/service/impl/ScenarioExecutorImpl.java
+++ src/main/java/executor/service/service/impl/ScenarioExecutorImpl.java
@@ -1,12 +1,43 @@
 package executor.service.service.impl;
 
 import executor.service.model.Scenario;
+import executor.service.model.Step;
 import executor.service.service.ScenarioExecutor;
+import executor.service.service.StepExecution;
 import org.openqa.selenium.WebDriver;
+
+import java.util.List;
 
 public class ScenarioExecutorImpl implements ScenarioExecutor {
     @Override
     public void execute(Scenario scenario, WebDriver webDriver) {
+        webDriver.get("url");
+        List<Step> steps = scenario.getSteps();
+        for (Step step: steps) {
 
+            StepExecution executor = getStepExecutor(step.getAction());
+            executor.step(webDriver, step);
+
+        }
+        webDriver.close();
     }
+
+    private StepExecution getStepExecutor(String action) {
+        StepExecution executor = null;
+        switch (action) {
+            case "clickCss":
+                executor = new StepExecutionClickCss();
+                break;
+            case "clickXpath":
+                executor = new StepExecutionClickXpath();
+                break;
+            case "sleep":
+                executor = new StepExecutionSleep();
+                break;
+            default:
+                throw new IllegalArgumentException("Invalid step type: " + action);
+        }
+        return executor;
+    }
+
 }
