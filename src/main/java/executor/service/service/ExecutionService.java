b464f0f6190fb0191fe17cf880a6f846039f0659
ykotsiuba <jurok3x@gmail.com>
2023-09-06 16:57:39 +0300

    Added ExecutionService interface and implementation

--- src/main/java/executor/service/service/ExecutionService.java
+++ src/main/java/executor/service/service/ExecutionService.java
@@ -1,2 +1,8 @@
-package executor.service.service;public interface ExecutionService {
+package executor.service.service;
+
+import executor.service.model.ProxyConfigHolder;
+import executor.service.model.Scenario;
+
+public interface ExecutionService {
+    void execute(Scenario scenario, ProxyConfigHolder proxy);
 }
--- src/main/java/executor/service/service/impl/ExecutionServiceImpl.java
+++ src/main/java/executor/service/service/impl/ExecutionServiceImpl.java
@@ -1,2 +1,34 @@
-package executor.service.service.impl;public class ExecutionServiceImpl {
+package executor.service.service.impl;
+
+import executor.service.model.ProxyConfigHolder;
+import executor.service.model.Scenario;
+import executor.service.model.WebDriverConfig;
+import executor.service.service.ExecutionService;
+import executor.service.service.ScenarioExecutor;
+import org.openqa.selenium.WebDriver;
+import org.slf4j.Logger;
+
+public class ExecutionServiceImpl implements ExecutionService {
+    private static final Logger log = LoggerFactory.getLogger(ExecutionServiceImpl.class);
+
+    private final ScenarioExecutor scenarioExecutor;
+    private final WebDriverConfig webDriverConfig;
+
+    public ExecutionServiceImpl(ScenarioExecutor scenarioExecutor, WebDriverConfig webDriverConfig) {
+        this.scenarioExecutor = scenarioExecutor;
+        this.webDriverConfig = webDriverConfig;
+    }
+
+    @Override
+    public void execute(Scenario scenario, ProxyConfigHolder proxy) {
+        WebDriver webDriver = getWebDriverPrototype(webDriverConfig, proxy);
+        if (webDriver == null) return;
+
+        scenarioExecutor.execute(scenario, webDriver);
+    }
+
+    private WebDriver getWebDriverPrototype(WebDriverConfig webDriverConfig, ProxyConfigHolder proxyConfigHolder) {
+        WebDriverInitializer webDriverInitializer = new WebDriverInitializerImpl();
+        return webDriverInitializer.getInstance(webDriverConfig, proxyConfigHolder);
+    }
 }
