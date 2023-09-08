e69f08fa5c05386a9dc6397a59879d18086c0b3d
Olexandr Tuleninov <talextu1@gmail.com>
2023-09-08 10:10:02 +0300

    added class for creating bean from properties file

--- src/main/java/executor/service/config/properties/PropertiesConstants.java
+++ src/main/java/executor/service/config/properties/PropertiesConstants.java
@@ -17,4 +17,9 @@ public final class PropertiesConstants {
     public static final String KEEP_ALIVE_TIME = "keep.alive.time";
     public static final String COMMONS_CONFIGURATION_PROPERTIES = "web-driver.properties";
 
+
+    public static final String PROXY_NETWORK = "proxy-network.json";
+    public static final String PROXY_NETWORK_DEFAULT = "proxy-network-default.json";
+    public static final String PROXY_CREDENTIALS = "proxy-credentials.json";
+    public static final String PROXY_CREDENTIALS_DEFAULT = "proxy-credentials-default.json";
 }
