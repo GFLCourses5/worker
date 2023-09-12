47d14ce8fc21afa1dc3bb9a9451bb63d3dbedab8
Olexandr Tuleninov <talextu1@gmail.com>
2023-09-12 15:51:44 +0300

    replaced StepExecutionFactory.java with ScenarioExecutorImpl.java

---
+++ src/main/java/executor/service/service/impl/ScenarioExecutorImpl.java
@@ -0,0 +1,51 @@
+package executor.service.service.impl;
+
+import executor.service.model.Scenario;
+import executor.service.model.Step;
+import executor.service.service.ScenarioExecutor;
+import executor.service.service.StepExecutionClickCss;
+import executor.service.service.StepExecutionClickXpath;
+import executor.service.service.StepExecutionSleep;
+import org.openqa.selenium.WebDriver;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+/**
+ * Selects a StepExecution for each Step and runs it..
+ *
+ *  @author Oleksandr Tuleninov
+ *  @version 01
+ * */
+public class ScenarioExecutorImpl implements ScenarioExecutor {
+
+    private static final Logger log = LoggerFactory.getLogger(ScenarioExecutorImpl.class);
+
+    private final StepExecutionClickCss stepExecutionClickCss;
+    private final StepExecutionSleep stepExecutionSleep;
+    private final StepExecutionClickXpath stepExecutionClickXpath;
+
+    public ScenarioExecutorImpl(StepExecutionClickCss stepExecutionClickCss,
+                                StepExecutionSleep stepExecutionSleep,
+                                StepExecutionClickXpath stepExecutionClickXpath) {
+        this.stepExecutionClickCss = stepExecutionClickCss;
+        this.stepExecutionSleep = stepExecutionSleep;
+        this.stepExecutionClickXpath = stepExecutionClickXpath;
+    }
+
+    @Override
+    public void execute(Scenario scenario, WebDriver webDriver) {
+        webDriver.get(scenario.getSite());
+
+        for (Step step : scenario.getSteps()) {
+            String action = step.getAction().getName();
+            switch (action) {
+                case "clickCss" -> stepExecutionClickCss.step(webDriver, step);
+                case "sleep" -> stepExecutionClickXpath.step(webDriver, step);
+                case "clickXpath" -> stepExecutionSleep.step(webDriver, step);
+                default -> log.error("Invalid step action: " + action);
+            }
+        }
+
+        webDriver.close();
+    }
+}
