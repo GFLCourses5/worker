577a82ba5b2623340ce58a64f342801135c9b815
Olexandr Tuleninov <talextu1@gmail.com>
2023-09-28 01:26:56 +0300

    copied StepExecutionClickXpath.java from spring branch to team branch

--- src/main/java/executor/service/service/impl/StepExecutionClickXpathImpl.java
+++ src/main/java/executor/service/service/impl/StepExecutionClickXpathImpl.java
@@ -3,17 +3,25 @@ package executor.service.service.impl;
 import executor.service.model.Step;
 import executor.service.model.StepTypes;
 import executor.service.service.StepExecutionClickXpath;
+import org.openqa.selenium.By;
+import org.openqa.selenium.NoSuchElementException;
 import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
 
 public class StepExecutionClickXpathImpl implements StepExecutionClickXpath {
 
-  @Override
-  public String getStepAction() {
-    return StepTypes.CLICK_XPATH.getName();
-  }
+    @Override
+    public String getStepAction() {
+        return StepTypes.CLICK_XPATH.getName();
+    }
 
-  @Override
-  public void step(WebDriver webDriver, Step step) {
-
-  }
+    @Override
+    public void step(WebDriver webDriver, Step step) {
+        try {
+            WebElement element = webDriver.findElement(By.xpath(step.getValue()));
+            element.click();
+        } catch (NoSuchElementException e) {
+            System.out.println("this is error");
+        }
+    }
 }
