df06ef651d4a4ea7c5178d420a839b503277032d
Dima (IfLunatic) <dimasilenkoluna@gmail.com>
2023-10-02 10:36:26 +0300

    added DIFactoryTest.java

---
+++ src/test/java/executor/service/config/simpleDI/DIFactoryTest.java
@@ -0,0 +1,33 @@
+package executor.service.config.simpleDI;
+
+import executor.service.service.ParallelFlowExecutorService;
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertNotNull;
+
+
+/**
+ * Test class for testing the functionality of the {@code DIFactory} class
+ * This class contains unit tests to verify that {@code DIFactory} is working correctly.
+ *
+ * @author Dima Silenko, Oleksandr Tuleninov
+ * @version 01
+ */
+public class DIFactoryTest {
+    @Test
+    public void testParallelFlowExecutorService() {
+        DIFactory diFactory = new DIFactory();
+        var service1 = diFactory.createObject(ParallelFlowExecutorService.class);
+        assertNotNull(service1);
+
+        // checking for singleton
+        var service2 = diFactory.createObject(ParallelFlowExecutorService.class);
+        assertNotNull(service2);
+        assertEquals(service1, service2);
+
+        //check that singleton using cache
+        var objectFromCache = diFactory.createObject(ParallelFlowExecutorService.class);
+        assertEquals(service1, objectFromCache);
+    }
+}
