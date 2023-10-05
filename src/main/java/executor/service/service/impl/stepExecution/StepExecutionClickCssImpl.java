d5421a137d5177c1a3018e5ac50c11cffc28cd67
Olexandr Tuleninov <talextu1@gmail.com>
2023-10-05 17:09:04 +0300

    refactored StepExecutionClickCssImpl.java, StepExecutionClickXpathImpl.java, StepExecutionSleepImpl.java (added JavaDoc and logging)

--- src/main/java/executor/service/service/impl/stepExecution/StepExecutionClickCssImpl.java
+++ src/main/java/executor/service/service/impl/stepExecution/StepExecutionClickCssImpl.java
@@ -7,22 +7,35 @@ import org.openqa.selenium.By;
 import org.openqa.selenium.NoSuchElementException;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 
+/**
+ * The {@code StepExecutionClickCssImpl} class implements the {@link StepExecutionClickCss} interface
+ * for executing a step that involves clicking an HTML element by CSS selector.
+ * <p>
+ *
+ * @author Anton Sokolsky
+ * @version 01
+ * @see executor.service.model.StepTypes
+ */
 public class StepExecutionClickCssImpl implements StepExecutionClickCss {
 
-  @Override
-  public String getStepAction() {
-    return StepTypes.CLICK_CSS.getName();
-  }
+    private static final Logger log = LoggerFactory.getLogger(StepExecutionSleepImpl.class);
 
-  @Override
-  public void step(WebDriver webDriver, Step step) {
-    try {
-      WebElement element = webDriver.findElement(By.cssSelector(step.getValue()));
-      element.click();
-    } catch (NoSuchElementException e ) {
-      System.out.println("this is error");
+    @Override
+    public String getStepAction() {
+        return StepTypes.CLICK_CSS.getName();
     }
 
-  }
+    @Override
+    public void step(WebDriver webDriver, Step step) {
+        String value = step.getValue();
+        try {
+            WebElement element = webDriver.findElement(By.cssSelector(value));
+            element.click();
+        } catch (NoSuchElementException e) {
+            log.error("Invalid step value: " + value);
+        }
+    }
 }
