0def37c5510f934564bcdfa6ed2d7572aa877b45
Sokol3783 <sokolplus3783@gmail.com>
2023-10-17 21:26:41 +0300

    add body of test

---
+++ src/test/java/executor/service/security/SecurityTest.java
@@ -0,0 +1,46 @@
+package executor.service.security;
+
+
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.access.SecurityConfig;
+import org.springframework.test.context.ContextConfiguration;
+import org.springframework.test.context.junit.jupiter.SpringExtension;
+import org.springframework.test.context.web.WebAppConfiguration;
+import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.test.web.servlet.setup.MockMvcBuilders;
+import org.springframework.web.context.WebApplicationContext;
+
+import static org.springframework.security.test.web.servlet.setup.SecurityMockMvcConfigurers.springSecurity;
+
+@ExtendWith(SpringExtension.class)
+@ContextConfiguration(classes = SecurityConfig.class)
+@WebAppConfiguration
+public class SecurityTest {
+
+    @Autowired
+    private WebApplicationContext context;
+
+    private MockMvc mvc;
+
+    @BeforeEach
+    public void setup() {
+        mvc = MockMvcBuilders
+                .webAppContextSetup(context)
+                    .apply(springSecurity())
+                .build();
+    }
+
+    @Test
+    public void isPermittedAddress(){
+
+    }
+
+    @Test
+    public void isNotPermittedAddress(){
+
+    }
+
+}
