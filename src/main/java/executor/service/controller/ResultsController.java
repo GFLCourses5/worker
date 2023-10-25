cd977c7f833b2faa913e4d3af3a0eaf0532f5862
Sokol3783 <sokolplus3783@gmail.com>
2023-10-25 23:36:28 +0300

    add simple result controller

--- src/main/java/executor/service/controller/ResultsController.java
+++ src/main/java/executor/service/controller/ResultsController.java
@@ -1,10 +1,35 @@
 package executor.service.controller;
 
-import org.springframework.web.bind.annotation.RestController;
+import executor.service.model.response.ScenarioResultResponse;
+import org.springframework.http.HttpStatusCode;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
 
 @RestController
 public class ResultsController {
 
 
+    @GetMapping
+    public ResponseEntity<ScenarioResultResponse> getResultByScenario(@RequestParam Integer scenarioId,
+                                                                      @RequestParam Integer userId) {
+        return ResponseEntity.ok(getEmptyResult());
+    }
+
+    private ScenarioResultResponse getEmptyResult() {
+        return new ScenarioResultResponse(null,null,null,null, null);
+    }
+
+    @GetMapping(value = "/list")
+    public ResponseEntity<List<ScenarioResultResponse>> getResults() {
+        return ResponseEntity.ok(List.of(getEmptyResult(),getEmptyResult()));
+    }
+
+    @DeleteMapping
+    public ResponseEntity<HttpStatusCode> deleteScenario(@RequestParam Integer scenarioId,
+                                                         @RequestParam Integer userId) {
+        return new ResponseEntity<>(HttpStatusCode.valueOf(202));
+    }
 
 }
