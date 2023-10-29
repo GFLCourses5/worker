a55a4db719e6aecc389a258ac9b9a289f72aa35d
Sokol3783 <sokolplus3783@gmail.com>
2023-10-29 13:45:50 +0200

    add controller test

---
+++ src/test/java/executor/service/controller/ScenarioSourceControllerTest.java
@@ -0,0 +1,27 @@
+package executor.service.controller;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+class ScenarioSourceControllerTest {
+
+    @Test
+    void receiveScenarios() {
+    }
+
+    @Test
+    void getScenariosResult() {
+    }
+
+    @Test
+    void testGetScenariosResult() {
+    }
+
+    @Test
+    void getScenarioById() {
+    }
+
+    @Test
+    void deleteResultById() {
+    }
+}
