10a89953dcc1814663556ae4890395f1b3d9ef80
Sokol3783 <sokolplus3783@gmail.com>
2023-10-29 21:54:20 +0200

    write some tests

--- src/test/java/executor/service/controller/ScenarioSourceControllerTest.java
+++ src/test/java/executor/service/controller/ScenarioSourceControllerTest.java
@@ -1,17 +1,39 @@
 package executor.service.controller;
 
+import com.fasterxml.jackson.databind.ObjectMapper;
+import executor.service.exceptions.ScenarioResultExceptions;
+import executor.service.model.Step;
+import executor.service.model.request.Scenario;
+import executor.service.model.request.ScenariosRequest;
+import executor.service.model.request.StepRequest;
+import executor.service.model.response.ScenarioResultResponse;
+import executor.service.model.response.StepResultResponse;
 import executor.service.service.ScenarioOperations;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
 import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
 import org.springframework.boot.test.mock.mockito.MockBean;
+import org.springframework.http.MediaType;
 import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.test.web.servlet.ResultMatcher;
 
-import static org.junit.jupiter.api.Assertions.*;
+import java.time.OffsetDateTime;
+import java.util.List;
+import java.util.Optional;
+
+import static executor.service.Routes.SCENARIOS;
+import static org.mockito.ArgumentMatchers.any;
+import static org.mockito.Mockito.doNothing;
+import static org.mockito.Mockito.when;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.delete;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.jsonPath;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
 
 @WebMvcTest(ScenarioSourceController.class)
-@AutoConfigureMockMvc
+@AutoConfigureMockMvc(addFilters = false)
 class ScenarioSourceControllerTest {
 
     @Autowired
@@ -20,9 +42,24 @@ class ScenarioSourceControllerTest {
     @MockBean
     private ScenarioOperations service;
 
-    @Test
-    void receiveScenarios() {
+    private ObjectMapper objectMapper = new ObjectMapper();
 
+    private final static OffsetDateTime now = OffsetDateTime.now();
+
+    @Test
+    void receiveNotValidScenarios() throws Exception {
+        doNothing().when(service).execute(any(ScenariosRequest.class));
+        mvc.perform(post(SCENARIOS).contentType(MediaType.APPLICATION_JSON).content(
+                objectMapper.writeValueAsString(getNotValidScenarioRequest())
+        )).andExpect(status().isBadRequest());
+    }
+
+    @Test
+    void receiveValidScenarios() throws Exception {
+        doNothing().when(service).execute(any(ScenariosRequest.class));
+        mvc.perform(post(SCENARIOS).contentType(MediaType.APPLICATION_JSON).content(
+                objectMapper.writeValueAsString(getValidScenarioRequest())
+        )).andExpect(status().isOk());
     }
 
     @Test
@@ -34,10 +71,54 @@ class ScenarioSourceControllerTest {
     }
 
     @Test
-    void getScenarioById() {
+    void getScenarioByIdIfPresent() throws Exception {
+
+        StepResultResponse stepResultResponse = new StepResultResponse(new Step("action", "value"), true);
+
+        when(service.getScenarioResultById(any(Long.class))).thenReturn(Optional.of(getScenarioResultResponse(stepResultResponse)));
+        mvc.perform(get(SCENARIOS+ "/1")).andExpect(status().isOk())
+                .andExpect(jsonPath("$.name").value("name"))
+                .andExpect(jsonPath("$.site").value("site"))
+                .andExpect(jsonPath("$.executedAt").value(now.toString()))
+                .andExpect(jsonPath("$.stepsResults").isArray())
+                .andExpect(validResponse("$.stepsResults[0]", stepResultResponse));
+    }
+
+    public static ResultMatcher validResponse(String prefix, StepResultResponse stepResultResponse) {
+        return ResultMatcher.matchAll(
+                jsonPath(prefix + ".step.value").value(stepResultResponse.step().getValue()),
+                jsonPath(prefix + ".step.action").value(stepResultResponse.step().getAction()),
+                jsonPath(prefix + ".isPassed").value(stepResultResponse.isPassed()));
     }
 
     @Test
-    void deleteResultById() {
+    void getScenarioByIdIfNotPresent() throws Exception {
+
+       when(service.getScenarioResultById(2l)).thenReturn(Optional.empty());
+        mvc.perform(get(SCENARIOS+ "/2")).andExpect(status().isNotFound());
+
     }
+
+    @Test
+    void deleteResultById() throws Exception {
+        doNothing().when(service).deleteById(any(Long.class),any(Long.class));
+        mvc.perform(delete(SCENARIOS+ "/1?userId=1")).andExpect(status().isOk());
+    }
+
+    private ScenariosRequest getNotValidScenarioRequest() {
+        List<Scenario> scenarios = List.of(new Scenario());
+        return new ScenariosRequest(scenarios,1, true );
+    }
+
+    private ScenariosRequest getValidScenarioRequest() {
+        List<StepRequest> stepRequests = List.of(new StepRequest("action", "value"));
+        List<Scenario> scenarios = List.of(new Scenario("name", "site", stepRequests));
+        return new ScenariosRequest(scenarios,1, true );
+    }
+
+    private ScenarioResultResponse getScenarioResultResponse(StepResultResponse stepResultResponse) {
+        List<StepResultResponse> result = List.of(stepResultResponse);
+        return  new ScenarioResultResponse(1, "name", "site", result, now);
+    }
+
 }
