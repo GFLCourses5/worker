f7455a6cff911da008843775291d068f903ea514
Sokol3783 <sokolplus3783@gmail.com>
2023-10-29 13:48:24 +0200

    add annotations and mock service

--- src/test/java/executor/service/controller/ScenarioSourceControllerTest.java
+++ src/test/java/executor/service/controller/ScenarioSourceControllerTest.java
@@ -1,12 +1,28 @@
 package executor.service.controller;
 
+import executor.service.service.ScenarioOperations;
 import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
+import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
+import org.springframework.boot.test.mock.mockito.MockBean;
+import org.springframework.test.web.servlet.MockMvc;
 
 import static org.junit.jupiter.api.Assertions.*;
+
+@WebMvcTest(ScenarioSourceController.class)
+@AutoConfigureMockMvc
 class ScenarioSourceControllerTest {
+
+    @Autowired
+    private MockMvc mvc;
+
+    @MockBean
+    private ScenarioOperations service;
 
     @Test
     void receiveScenarios() {
+
     }
 
     @Test
